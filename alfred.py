2388096db6779df28309542935b4b1f615f631d5
root <franco.quijano@gmail.com>
2018-12-27 20:53:13 +0000

    primera condicional

--- alfred.py
+++ alfred.py
@@ -12,14 +12,22 @@ parser.read('alfred.conf')
 AUTHUSER=parser.get('AWS', 'User')
 AUTHPASS=parser.get('AWS', 'Pass')
 BUCKET=parser.get('AWS', 'Bucket')
-ARCHIVO=str(sys.argv[1])
-PATH_DESTINO=str(sys.argv[2])
+COMANDO=str(sys.argv[1])
+ARCHIVO=str(sys.argv[2])
+PATH_DESTINO=str(sys.argv[3])
 
 print ('el AUTHUSER  es ' + AUTHUSER)
 print ('el AUTHPASS es ' + AUTHPASS)
 print ('el bucket es ' + BUCKET)
 print ('el archivo origen se llama ' + ARCHIVO)
 print ('el path destino es ' + PATH_DESTINO)
+print('')
+if COMANDO == 'descargar':
+    if ARCHIVO == 'dbdeldia':
+        print ('Descaragando la db de hoy')
+    else:
+        print ('Descaragando la db ' + ARCHIVO)
+    
 
 #S3_OBJECT = boto3.client('s3', region_name='us-east-1', aws_access_key_id='AWS_ACCESS_KEY', aws_secret_access_key='AWS_SECRETY_KEY')
 
