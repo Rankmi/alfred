79cc948a0819d258047b1d68c4561d1a4faf07e1
Edgardo <edgardo.fredz@rankmi.com>
2019-01-28 15:47:20 -0300

    Updated README and refactoring

--- README.md
+++ README.md
@@ -1,41 +1,49 @@
 # alfred
-Prerequisitos
+Prerequisitos(el installer se encargará de ellos)
 - python3
+- pyenv
+- pipenv
 - pip3
 
-Instalar:
+## Instalación
 
-pip3 install boto3
+``` sh 
+$ sh installer.sh
+# o puedes usar
+$ ./installer.sh
+```  
 
 
-Pruebas de funcionamiento:
-- Se deben colocar las credenciales de AWS en el archivo alfred.conf
-El formato del archivo es 
+## Uso
 
+### Credenciales
+- La primera vez que la aplicación se ejecuta, la aplicación te solicitará las
+  credenciales.
+- En caso de que necesites modificar estas credenciales puedes hacerlo con el
+  siguiente comando(si prefieres modificarla localmente, puedes modificar el
+  archivo de configuración de alfred que está ubicado en ~/.alfred.conf):
+
+``` sh 
+  $ ./alfred reset credentials
 ```
-[AWS]
-User=AWS_ACCESS_KEY
-Pass=AWS_SECRET_KEY
 
-[DIARIOS]
-Bucket=nombre-del-bucket
-
-```
-
+### Descarga bases de datos
 Fase 1: Copia de base de datos diaria a local
 
 Las descargas se realizaran automaticamente en el directorio desde donde se ejecuta el script
 
 - Para descargar la ultima db:
-Ejemplo comando: `./alfred.py get todaydb`
+Ejemplo comando: `./alfred.sh get todaydb`
 
 - Para descargar una db de un dia especifico: (Se debe usar el formato de fecha de la siguiente forma: YYYY_MM_DD)
-ejemplo comando: `./alfred.py get 2019_01_01`
+ejemplo comando: `./alfred.sh get 2019_01_01`
 
 
 Fase 2: Crear DUMP de la base de datos de cualquier ambiente (en pruebas):
 
 - Para crear el dump:
-./alfred.py dump environment
+``` sh 
+./alfred.sh dump environment
+```
 
-Ejemplo: `./alfred.py dump production`
+Ejemplo: `./alfred.sh dump production`
--- alfred/main.py
+++ alfred/main.py
@@ -12,54 +12,41 @@ import datetime
 import getpass
 from alfredconfig import AlfredConfig
 from configfilehelper import config_file_exists, configlocation, set_config_file
+from tqdm import tqdm
+
+AWS_ACCESS_KEY = ""
+AWS_SECRET_KEY = ""
+BUCKET = ""
+
+#SELECCION DE PROCESO
+def parsearguments(command, key):
+    #PROCESO GET
+    if command == 'get':
+        getbackup(key, AWS_ACCESS_KEY, AWS_SECRET_KEY, BUCKET)
+    #PROCESO DUMP
+    elif command == 'dump':
+        dumpbackup(key, AWS_ACCESS_KEY, AWS_SECRET_KEY, BUCKET)
+    #RESET CREDENTIALS
+    elif command == 'reset':
+        if key == 'credentials':
+            askcredentials()
+    else:
+        print ('Command Unknown. Maybe you could try with get command')
+
+# Validación de largo de argumentos
+def validateargv():
+    if len(sys.argv) == 3:
+        global FILENAME
+        COMANDO=str(sys.argv[1])
+        KEY=str(sys.argv[2])
+        FILENAME = KEY
+        parsearguments(COMANDO, KEY)
+    else:
+        print("Debes ingresar al menos dos argumentos")
 
 
 
-def print_progress(iteration, total, prefix='', suffix='', decimals=1, bar_length=100):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        bar_length  - Optional  : character length of bar (Int)
-    """
-    str_format = "{0:." + str(decimals) + "f}"
-    percents = str_format.format(100 * (iteration / float(total)))
-    filled_length = int(round(bar_length * iteration / float(total)))
-    bar = '█' * filled_length + '*' * (bar_length - filled_length)
-
-    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percents, '%', suffix)),
-
-    if iteration == total:
-        sys.stdout.write('\n')
-    sys.stdout.flush()
-
-class ProgressPercentage(object):
-    def __init__(self, client, bucket, filename):
-        self._filename = filename
-        s3 = boto3.client('s3', region_name='us-west-2', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
-        self._size = s3.head_object(Bucket=bucket, Key=filename)['ContentLength']
-        self._seen_so_far = 0
-        self._lock = threading.Lock()
-    def __call__(self, bytes_amount):
-        with self._lock:
-            self._seen_so_far += bytes_amount
-            percentage = round((self._seen_so_far / self._size) * 100,2)
-            if int(percentage) % 1 == 0:
-                print_progress(int(percentage),100,"Downloading "+filename,"Completed",0,int(percentage))
-
-
-# Asking for user access keys
-if config_file_exists():
-    parser = ConfigParser()
-    parser.read(configlocation)
-    AWS_ACCESS_KEY=parser.get('AWS', 'User')
-    AWS_SECRET_KEY=parser.get('AWS', 'Pass')
-    BUCKET = parser.get('DIARIOS', 'Bucket')
-else:
+def askcredentials():
     AWS_ACCESS_KEY = input('User: ')
     AWS_SECRET_KEY = getpass.getpass('Password: ')
     BUCKET = input('Bucket (default = rankmi-backup-semanal) : ')
@@ -68,53 +55,46 @@ else:
     set_config_file(AlfredConfig(AWS_ACCESS_KEY, AWS_SECRET_KEY, BUCKET))
 
 
+def hook(t):
+  def inner(bytes_amount):
+    t.update(bytes_amount)
+  return inner
 
-#INICIALIZANDO VARIABLES
-COMANDO=str(sys.argv[1])
-KEY=str(sys.argv[2])
-
-#IMPRIMIR VARIABLES
-filename = KEY
-
-#SELECCION DE PROCESO
-
-
-
-#PROCESO GET
-if COMANDO == 'get':
-    resource = boto3.resource('s3', region_name='us-west-2', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
-    
-    if KEY == 'todaydb':
+def getbackup(key, awskey, awssecret, bucket):
+    resource = boto3.resource('s3', region_name='us-west-2', aws_access_key_id=awskey, aws_secret_access_key=awssecret)
+    if key == 'todaydb':
         filename = datetime.datetime.now().strftime('%Y_%m_%d')
-    
-    print ('Download  '+filename+' database')
-    progress = ProgressPercentage(resource, BUCKET, 'backup_reducido_' + filename + '.tar.xz')    
+
+    print ('Download ' + filename + ' database')
+    awsfile = 'backup_reducido_' + filename + '.tar.xz'
+    fileobject = resource.Object(bucket, awsfile)
+    filesize = fileobject.content_length
     try:
-        resource.Bucket(BUCKET).download_file('backup_reducido_' + filename + '.tar.xz','backup_reducido_' + KEY + '.tar.gz',Callback=progress)
+        FILE_NAME = "algo"
+        with tqdm(total=filesize, unit='B', unit_scale=True, desc=FILE_NAME) as t:
+            resource.Bucket(bucket).download_file(awsfile, 'backup_reducido_' + key + '.tar.gz',Callback=hook(t))
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
             print("There's no database... Try with the following format YYYY_MM_DD")
         else:
             raise
 
-
-#PROCESO DUMP
-elif COMANDO == 'dump':
-    if (KEY == 'production') or (KEY == 'staging') or (KEY == 'development'):
+def dumpbackup(key, awssecret, bucket):
+    if (key == 'production') or (key == 'staging') or (key == 'development'):
         #revisar si la maquina esta encendida, si lo esta solicitar intentar nuevamente, sino lo esta entonces encender
-        print ('Batman, the dump of the '+KEY+' environment is starting...')
+        print ('Batman, the dump of the '+key+' environment is starting...')
         instances = ['i-0b08005bfb8829080']
-        resource = boto3.client('ec2', region_name='us-west-2', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
+        resource = boto3.client('ec2', region_name='us-west-2', aws_access_key_id=awskey, aws_secret_access_key=awssecret)
         status=resource.describe_instance_status(InstanceIds=instances)
         if not status.get("InstanceStatuses"):
             print("Starting dump... Batman should wait aprox 30min to complete this asynchronous task...")
             print("To download this dump please use this command:")
             print("")
-            print("--->>>> main.py get "+KEY+"_DD_MM_YY")
+            print("--->>>> main.py get "+key+"_DD_MM_YY")
             print("")
-            s3 = boto3.resource('s3', region_name='us-west-2', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
+            s3 = boto3.resource('s3', region_name='us-west-2', aws_access_key_id=awskey, aws_secret_access_key=awssecret)
             object = s3.Object('rankmi-backup-semanal','backup-type.dat')
-            object.put(Body=KEY)
+            object.put(Body=key)
             resource.start_instances(InstanceIds=instances)
         else:
             print ("Hay otro Dump en proceso, por favor intentende nuevo en unos minutos...")
@@ -122,5 +102,18 @@ elif COMANDO == 'dump':
     else:
         print ('Batman, debes indicar el ambiente correctamente...')
 
-else:
-    print ('Command Unknown. Maybe you could try with get command')
+def readconfig():
+    global AWS_ACCESS_KEY, AWS_SECRET_KEY, BUCKET
+    parser = ConfigParser()
+    parser.read(configlocation)
+    AWS_ACCESS_KEY=parser.get('AWS', 'User')
+    AWS_SECRET_KEY=parser.get('AWS', 'Pass')
+    BUCKET = parser.get('DIARIOS', 'Bucket')
+
+if __name__ == '__main__':
+    if config_file_exists():
+        readconfig()
+    else:
+        askcredentials()
+    FILENAME = "default"
+    validateargv()
