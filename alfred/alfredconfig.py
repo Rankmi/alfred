81ea2a807ffafa31e7b4f38117d37c49e0d3fa1a
Edgardo <edgardo.fredz@rankmi.com>
2019-02-05 12:54:49 -0300

    Added string casting for path

--- alfred/alfredconfig.py
+++ alfred/alfredconfig.py
@@ -1,10 +1,5 @@
 class AlfredConfig:
-    user: str
-    password: str
-    bucket: str
-
     def __init__(self, user, password, bucket):
         self.user = user
         self.password = password
         self.bucket = bucket
-
