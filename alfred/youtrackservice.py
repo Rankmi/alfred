1e1d514189f1af7f1376dbfff98af610b10c24bd
Rodrigo Naranjo <rodnaranjop@gmail.com>
2019-02-20 10:09:35 -0300

    Select issue from list

--- alfred/youtrackservice.py
+++ alfred/youtrackservice.py
@@ -2,21 +2,26 @@ import json
 import datetime
 import requests
 
-from configfilehelper import get_config_key, YOUTRACK_SECTION, YOUTRACK_KEY, USER_KEY, \
+from .configfilehelper import get_config_key, YOUTRACK_SECTION, YOUTRACK_KEY, USER_KEY, \
     reset_youtrack_credentials
+from .printservice import print_issue
 
 __base_url = "https://rankmi.myjetbrains.com/youtrack/api/"
 
 
 def get_my_open_issues():
     params = dict(fields="project(shortName),numberInProject,summary",
-                  query="for: " + get_youtrack_user() + " #unresolved")
+                  query=get_youtrack_user() + " #unresolved")
     request_url = __base_url + "issues"
     user_request = requests.get(request_url, headers=get_header(), params=params)
     user_list = json.loads(user_request.text)
-    for user in user_list:
-        deserialized_user = User(user['summary'], user['numberInProject'], user['project'])
-        print(deserialized_user)
+    issues = []
+    for n in range(len(user_list)):
+        deserialized_user = User(user_list[n]['summary'], user_list[n]['numberInProject'], user_list[n]['project'])
+        issues.append(deserialized_user.project['shortName'] + "-" + str(deserialized_user.numberInProject))
+        print("["+str(n)+"]", deserialized_user)
+    print_issue(get_issue_by_id(issues[int(input("Ingresa el indice del ticket que deseas revisar: "))]))
+    
 
 
 def get_issue_by_id(id):
